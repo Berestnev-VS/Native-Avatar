f296174a9bf0da409985fe42e5484dfb419f9247
Berestnev Vladimir <berestnev.sv@gmail.com>
2023-05-23 22:33:00 +0300

    main code

--- Native Avatar/SceneDelegate.swift
+++ Native Avatar/SceneDelegate.swift
@@ -13,18 +13,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     
     
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        window = UIWindow(frame: UIScreen.main.bounds)
-        
         guard let windowScene = (scene as? UIWindowScene) else { return }
-        
         window = UIWindow(windowScene: windowScene)
         let viewController = ViewController()
         let navigationController = CustomNavController(rootViewController: viewController)
         window?.rootViewController = navigationController
         window?.makeKeyAndVisible()
-        
-
-        
     }
     
     func sceneDidDisconnect(_ scene: UIScene) {
--- Native Avatar/ViewController.swift
+++ Native Avatar/ViewController.swift
@@ -1,65 +1,54 @@
 import UIKit
 
 class ViewController: UIViewController, UIScrollViewDelegate {
+    
     let scrollView = UIScrollView()
-    let customTitleView: UIView = {
-        let view = UIView()
-        view.translatesAutoresizingMaskIntoConstraints = false
-        return view
-    }()
-    let titleLabel: UILabel = {
-        let label = UILabel()
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.font = UIFont.systemFont(ofSize: 34, weight: .bold)
-        label.text = "Avatar"
-        return label
-    }()
+    
     let imageView: UIImageView = {
         let iv = UIImageView()
         iv.translatesAutoresizingMaskIntoConstraints = false
-        iv.image = UIImage(systemName: "person.crop.circle.fill")
         iv.contentMode = .scaleAspectFit
+        iv.clipsToBounds = true
+        let symbolConfig = UIImage.SymbolConfiguration(pointSize: 36, weight: .medium)
+        iv.image = UIImage(systemName: "person.crop.circle.fill", withConfiguration: symbolConfig)
         return iv
     }()
     
+    func configureAvatar() {
+        guard let navigationLargeTitleView = NSClassFromString("_UINavigationBarLargeTitleView") else { return }
+        guard let navigationController = self.navigationController else { return }
+        navigationController.navigationBar.subviews.forEach { navSubview in
+            if navSubview.isKind(of: navigationLargeTitleView.self) {
+                navSubview.addSubview(imageView)
+                
+                NSLayoutConstraint.activate([
+                    imageView.bottomAnchor.constraint(equalTo: navSubview.bottomAnchor, constant: -5),
+                    imageView.trailingAnchor.constraint(equalTo: navSubview.trailingAnchor, constant: -10),
+                    imageView.widthAnchor.constraint(equalToConstant: 36),
+                    imageView.heightAnchor.constraint(equalToConstant: 36)
+                ])
+            }
+        }
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
-        
-        scrollView.delegate = self
+        title = "Avatar"
+        view.addSubview(scrollView)
+        navigationController?.navigationBar.prefersLargeTitles = true
         scrollView.frame = view.bounds
         scrollView.contentSize = CGSize(width: view.frame.width, height: 2000)
-        
-        view.addSubview(scrollView)
-        view.addSubview(customTitleView)
-        customTitleView.addSubview(titleLabel)
-        customTitleView.addSubview(imageView)
-        
-        NSLayoutConstraint.activate([
-            customTitleView.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor),
-            customTitleView.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor),
-            customTitleView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
-            
-            titleLabel.leadingAnchor.constraint(equalTo: customTitleView.leadingAnchor),
-            titleLabel.centerYAnchor.constraint(equalTo: customTitleView.centerYAnchor),
-            
-            imageView.trailingAnchor.constraint(equalTo: customTitleView.trailingAnchor, constant: 10),
-            imageView.centerYAnchor.constraint(equalTo: customTitleView.centerYAnchor),
-            imageView.widthAnchor.constraint(equalToConstant: 36),
-            imageView.heightAnchor.constraint(equalToConstant: 36),
-        ])
+        configureAvatar()
     }
     
-    func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        let offset = max(0, scrollView.contentOffset.y)
-        customTitleView.transform = CGAffineTransform(translationX: 0, y: -offset)
-    }
 }
 
 class CustomNavController: UINavigationController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        navigationBar.prefersLargeTitles = false
-        
+        navigationBar.prefersLargeTitles = true
     }
 }
+
+
